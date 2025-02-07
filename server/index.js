2de567053f37f668c473fa57fe9cb1a9fa794ccd
Krish9Patel <krishp185@outlook,com>
2025-02-07 02:12:29 -0500

    fixes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Starting server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
