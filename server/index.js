72636df91c2bd807aabca6577e3b650125566201
Krish9Patel <krishp185@outlook,com>
2025-02-11 14:00:13 -0500

    database implementation

--- server/index.js
+++ server/index.js
@@ -18,17 +18,18 @@ app.get('/', (req, res) => {
 });
 
 // Starting server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-mongoose.connect((process.env.MONGO_URI), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
+const mongoURI = process.env.MONGO_URL || process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Database connection error:', err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const taskRoutes = require('./routes/taskRoutes');
 app.use('/api/tasks', taskRoutes);
