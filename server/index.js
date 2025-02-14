edf5f1a5cecd923a884a930fc9c9cf75b83614f0
Krish9Patel <krishp185@outlook,com>
2025-02-14 01:57:33 -0500

    fixed the port issues

--- server/index.js
+++ server/index.js
@@ -16,15 +16,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
-app.listen(8080, "0.0.0.0", () => {
-  console.log("Server running on port 8080");
-});
-
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // MongoDB connection
 const mongoURI = process.env.MONGO_URL || process.env.MONGO_URI;
